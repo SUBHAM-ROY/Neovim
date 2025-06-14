2dc6d1cc2b7b8023962de5ba323757ac5b7c78b3
SUBHAM-ROY <>
2025-06-14 13:21:32 -0700

    Latest

--- init.lua
+++ init.lua
@@ -1,3 +1,2 @@
 require("config.lazy")
 require("config.options")
-vim.lsp.enable('ts_ls')
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,7 +1,9 @@
 vim.o.cursorline=true -- Show current line highlights
+vim.o.relativenumber=true -- Show relative numbers from current
 vim.o.splitright=true -- Split vertical on right
 vim.o.splitbelow=true -- Split horizontal on bottom
 vim.o.scrolloff=10 -- Always Keep 10 lines below current line for context
+vim.o.clipboard="unnamedplus" -- Shared clipboard
 
 vim.o.expandtab = true       -- Convert tabs to spaces
 vim.o.shiftwidth = 4         -- Use 4 spaces for autoindent
